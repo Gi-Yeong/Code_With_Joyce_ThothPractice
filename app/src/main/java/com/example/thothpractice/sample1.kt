01a7c03a1fed445f117015825de2d3538a81031a
kiyoung85 <kiyoung85@gmail.com>
2020-10-31 23:36:38 +0900

    For / While

--- app/src/main/java/com/example/thothpractice/sample1.kt
+++ app/src/main/java/com/example/thothpractice/sample1.kt
@@ -2,7 +2,7 @@ package com.example.thothpractice
 
 fun main() {
 
-    checkNum(1)
+    forAndWhile()
 
 }
 
@@ -27,10 +27,10 @@ fun checkNum(score: Int) {
         2, 3 -> println("this is 2 or 3")
         else -> println("I don't know")
     }
-    
+
     // 이때에는 else 를 꼭 써야 한다
     // Expression
-    var b = when(score) {
+    var b = when (score) {
         1 -> 1
         2 -> 2
         else -> 3
@@ -38,7 +38,7 @@ fun checkNum(score: Int) {
 
     println("b : ${b}")
 
-    when(score) {
+    when (score) {
         in 90..100 -> println("You are genius")
         in 10..80 -> println("not bad")
         else -> println("okay")
@@ -69,5 +69,33 @@ fun array() {
     arrayList.add(10)
     arrayList.add(20)
 
+}
 
+// 6. For / While
+
+fun forAndWhile() {
+
+    val student = arrayListOf("joyce", "james", "jenny", "jennifer")
+
+    for (name in student) {
+        println("${name}")
+    }
+
+    for ((index, name) in student.withIndex()) {
+        println("${index + 1} 번째 학색: ${name}")
+    }
+
+    var sum: Int = 0
+//    for (i in 1..10 step 2) {
+//    for (i in 10 downTo 1) {
+    for (i in 1 until 100) { // 1부터 99 까지
+        sum += i
+    }
+    println(sum)
+
+    var index = 0
+    while (index < 10) {
+        println("current index: ${index}")
+        index++
+    }
 }
