317972d86cc6f00916a7725cf8b9abe4d3fd76ab
kiyoung85 <kiyoung85@gmail.com>
2020-10-31 23:27:10 +0900

    Array and List

--- app/src/main/java/com/example/thothpractice/sample1.kt
+++ app/src/main/java/com/example/thothpractice/sample1.kt
@@ -49,3 +49,25 @@ fun checkNum(score: Int) {
 // 1. 무언가 뽀짝뽀짝 해서 값을 만들면 Expression
 // 2. 그게 아니면 Statement
 // 기본적으로 코틀린의 함수는 다 Expression 이다. 리턴 값이 없어 보여도 Unit 을 리턴 하기 때문에
+
+// 5. Array and List
+
+// Array -> 크기가 할당 되어 나옴
+// List 1. List 2. MutableList
+
+fun array() {
+    val array = arrayOf(1, 2, 3)
+    val list = listOf(1, 2, 3)
+
+    val array2 = arrayOf(1, "d", 3.4f)
+    val list2 = listOf(1, "d", 11L)
+
+    array[0] = 3
+    var result = list.get(0)
+
+    var arrayList = arrayListOf<Int>()
+    arrayList.add(10)
+    arrayList.add(20)
+
+
+}
