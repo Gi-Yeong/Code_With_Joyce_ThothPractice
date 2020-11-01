3113a3e3bbfa64397c52f0d1080c1838afc2c1e2
kiyoung85 <kiyoung85@gmail.com>
2020-11-02 00:07:08 +0900

    object

---
+++ app/src/main/java/com/example/thothpractice/FourthPractice.kt
@@ -0,0 +1,23 @@
+package com.example.thothpractice
+
+// Singleton Pattern
+object CarFactory {
+    val cars = mutableListOf<Car>()
+
+    fun makeCar(horsePower: Int): Car {
+        var car = Car(horsePower)
+        cars.add(car)
+        return car
+    }
+}
+
+data class Car(val horsePower: Int)
+
+fun main() {
+    val car = CarFactory.makeCar(10)
+    val car2 = CarFactory.makeCar(200)
+
+    println(car)
+    println(car2)
+    println(CarFactory.cars.size)
+}
